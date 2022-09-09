7550608a14dc1d4a99e1258aaf7fe4cb39bfb46a
Sturgill <davidsturgill@nci-02218892-m.nci.nih.gov>
2022-09-09 16:46:53 -0400

    First Commit

---
+++ R/bigESS.R
@@ -0,0 +1,33 @@
+#' Big ESS function
+#'
+#' For a given sce, choose the cell types, aggregation method, and specificity method.  Return either vector or matrix
+#'
+#' @param x An sce object
+#' @param celltypes A vector of cell type labels
+#' @param addmethod The method for aggregating counts
+#' @param specmethod The method for specificity
+#'
+#' @return A either a matrix or a vector, depending on the method used
+#'
+#' @importFrom edgeR Matrix.utils
+#' @export
+bigESS <- function(sce,celltypes,aggmethod,specmethod) {
+  
+  # REduce sce object to cell types of interest
+  mysce <- sce[,meta("label",sce) %in% celltypes]
+  # Aggregate counts by celltype
+  aggr_counts <- t(aggregate.Matrix(t(counts(mysce)), groupings = factor(meta("label",mysce)), fun = aggmethod) )
+  x <- as.matrix(aggr_counts)
+  
+  # Now do the specificity
+  if (specmethod == "Arda") {
+    mySums <- rowSums(x) 
+    essmat <- x/mySums
+    return(essmat)
+  }
+  if (specmethod == "gini") {
+    return(gini(t(x)))
+  }
+  
+  
+}
